defe9f11dda8b2baa5da40f142c25849701a6274
吴文旭 <96489927+algorithmwu@users.noreply.github.com>
2022-06-16 21:12:23 +0800

    Update Code00_HelloWorld.java

--- src/class00/Code00_HelloWorld.java
+++ src/class00/Code00_HelloWorld.java
@@ -3,5 +3,6 @@ package class00;
 public class Code00_HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("pull test!");
     }
 }
