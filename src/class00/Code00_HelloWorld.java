225702b55d73462f4c3f9347717224e1ae95f9ff
algorithmWu <wenxuwu15@outlook.com>
2022-06-16 21:14:16 +0800

    test

--- src/class00/Code00_HelloWorld.java
+++ src/class00/Code00_HelloWorld.java
@@ -3,6 +3,5 @@ package class00;
 public class Code00_HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("pull test!");
     }
 }
